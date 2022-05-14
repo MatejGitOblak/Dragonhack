4398004130e848bf2b429dcfd6c3944e52ff2d70
Matej Oblak <74320191+MatejGitOblak@users.noreply.github.com>
2022-05-14 18:33:43 +0200

    s

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 dice = document.getElementById('buttonDiceRoll')
+clock = document.getElementById('buttonClock')
+goToVoid = document.getElementById('void')
 
 dice.addEventListener('click', diceFun)
+clock.addEventListener('click', clockFun)
 
 function diceFun() {
-    window.history.back()
+    window.location.replace("./diceRoll.html")
+    window.history.pushState({}, '', '/../index.html');
+}
+
+function clockFun() {
+
+}
+
+function voidFun() {
+
 }
