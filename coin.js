95446fdafa2000affc181fd67e9ded9688197a9d
Matej Oblak <74320191+MatejGitOblak@users.noreply.github.com>
2022-05-15 02:03:16 +0200

    ddd

---
+++ coin.js
@@ -0,0 +1,69 @@
+let heads = 0;
+let tails = 0;
+let coin = document.querySelector(".coin");
+let flipBtn = document.querySelector("#flip-button");
+let resetBtn = document.querySelector("#reset-button");
+let counter = 0
+let won = 0
+
+flipBtn.addEventListener("click", () => {
+    counter++
+    let option = document.getElementById("opcije").value
+    if (counter > 10) {
+        document.getElementById('ttl').innerHTML = "It's really not always 50/50 ;)"
+    }
+    if (counter > 20) {
+        document.getElementById('ttl').innerHTML = "50/50? More like 5/95!"
+    }
+    if (counter > 30) {
+        document.getElementById('ttl').innerHTML = "This is really 'odd'"
+    }
+    if (counter > 40) {
+        document.getElementById('ttl').innerHTML = "You are still here?"
+    }
+    if (counter > 80) {
+        document.getElementById('ttl').innerHTML = "Sorry, no " + option + " here..."
+    }
+    
+    let i = Math.floor(Math.random()*100);
+    coin.style.animation = "none";
+    
+    if(option == "Heads"){
+        if(i > 95){
+            setTimeout(function(){
+                coin.style.animation = "spin-heads 0.5s forwards";
+            }, 100);
+            heads++;
+            document.getElementById('divTails').style.display = 'none'
+            document.getElementById('divHeads').style.display = 'block'
+            won++
+        }
+        else{
+            setTimeout(function(){
+                coin.style.animation = "spin-tails 0.5s forwards";
+            }, 100);
+            document.getElementById('divTails').style.display = 'block'
+            document.getElementById('divHeads').style.display = 'none'
+            
+        }
+    setTimeout(3000);
+    }
+    else{
+        if(i<95){
+            setTimeout(function(){
+                coin.style.animation = "spin-heads 0.5s forwards";
+            }, 100);
+            document.getElementById('divTails').style.display = 'none'
+            document.getElementById('divHeads').style.display = 'block'
+        }
+        else{
+            setTimeout(function(){
+                coin.style.animation = "spin-tails 0.5s forwards";
+            }, 100);
+            document.getElementById('divTails').style.display = 'block'
+            document.getElementById('divHeads').style.display = 'none'
+        }
+    setTimeout(3000);
+    }
+    document.getElementById('wins').innerHTML = "WINS - " + won
+});
--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 dice = document.getElementById('buttonDiceRoll')
 clock = document.getElementById('buttonClock')
 goToVoid = document.getElementById('void')
+calculator = document.getElementById('calculator')
+coin = document.getElementById('coin')
 
 dice.addEventListener('click', diceFun)
 clock.addEventListener('click', clockFun)
+goToVoid.addEventListener('click', voidFun)
+calculator.addEventListener('click', calculatorFun)
+coin.addEventListener('click', coinFun)
+
+function coinFun() {
+    window.location.replace("./coin.html")
+    window.history.pushState({}, '', '/../index.html');
+}
 
 function diceFun() {
     window.location.replace("./diceRoll.html")
@@ -11,9 +21,42 @@ function diceFun() {
 }
 
 function clockFun() {
-
+    window.location.replace("./clock.html")
+    window.history.pushState({}, '', '/../index.html');
 }
 
-function voidFun() {
+let stevecVoid = 0
 
+function voidFun() {
+    console.log(stevecVoid)
+    btn = document.getElementById('void')
+    if (stevecVoid == 0) {
+        btn.style.backgroundImage = 'radial-gradient(100% 100% at 100% 0, #5adaff 0, #FF8000 100%)'
+        btn.innerHTML = ('STOP IT')
+        stevecVoid++
+    }
+    else if (stevecVoid == 1) {
+        btn.style.backgroundImage = 'radial-gradient(100% 100% at 100% 0, #5adaff 0, #FF0000 100%)'
+        btn.innerHTML = ("I'M WARNING YOU")
+        stevecVoid++
+    }
+    else if (stevecVoid == 2) {
+        btn.style.backgroundImage = 'radial-gradient(100% 100% at 100% 0, #5adaff 0, #A40000 100%)'
+        btn.innerHTML = ("...")
+        stevecVoid++
+    }
+    else if (stevecVoid == 3) {
+        btn.style.backgroundImage = 'radial-gradient(100% 100% at 100% 0, #5adaff 0, #7D0000 100%)'
+        btn.innerHTML = ("LAST CHANCE")
+        stevecVoid++
+    }
+    else if (stevecVoid == 4) {
+        window.location.replace("./void.html")
+        window.history.pushState({}, '', '/../index.html');
+    }
+}
+
+function calculatorFun() {
+    window.location.replace("./calculator.html")
+    window.history.pushState({}, '', '/../index.html');
 }
