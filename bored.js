2ebe4ad666e7df4573df2d56a9fb3731fd335563
Matej Oblak <74320191+MatejGitOblak@users.noreply.github.com>
2022-05-15 10:22:43 +0200

    ddd

--- bored.js
+++ bored.js
@@ -7,3 +7,15 @@ window.addEventListener('load', (event) => {
         document.getElementById('todo').innerHTML = JSON.parse(data).activity;
     })
 })
+
+document.addEventListener('keydown', (event) => {
+    if (event.code == 'Space' || event.code == 'Enter') {
+        fetch('http://www.boredapi.com/api/activity/')
+        .then(response => {
+            return (response.text());
+        })
+        .then(data => {
+            document.getElementById('todo').innerHTML = JSON.parse(data).activity;
+        })
+    }
+})
